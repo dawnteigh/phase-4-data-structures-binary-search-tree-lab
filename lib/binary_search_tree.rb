9e5c237811120b6259add208ca692f2baddd42e9
Donte Ladatto <donte279@gmail.com>
2022-10-22 12:48:20 -0500

    commit tree d373f7ea02a1460749d6aba49ce9246991c3451a

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,46 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    node = @root
+    if node
+      until node.nil? || node.value == value
+        if node.value > value
+          node = node.left
+        else
+          node = node.right
+        end
+      end
+      node
+    end
   end
 
   def insert(value)
-    # your code here
+    node = Node.new(value)
+    if @root.nil?
+      @root = node
+    end
   end
-
-end 
+#   def insert(value)
+#     node = Node.new(value)
+#     if @root.nil?
+#       @root = node
+#     end
+#     if !search(node.value)
+#       curr = @root
+#       while node.value > curr.value || node.value < curr.value
+#         if node.value > curr.value
+#           if curr.right.nil?
+#             curr.right = node
+#           end
+#           curr = curr.right
+#         else
+#           if curr.left.nil?
+#             curr.left = node
+#           end
+#           curr = curr.left
+#         end
+#       end
+#       node
+#     end
+#   end
+end
